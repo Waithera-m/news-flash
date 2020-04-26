9606099e78afde01393dc8e2831d6c4a3b29a4e2
Mary Waithera <njihiamary11@gmail.com>
2020-04-26 20:05:25 +0300

    Remove session functions due to numerous errors, I will introduce them again after further research

--- app/main/views.py
+++ app/main/views.py
@@ -14,28 +14,6 @@ def index():
     title = 'News-Flash'
     return render_template('index.html',title = title,sources = news_sources,headlines = top_headlines)
     
-    if 'username' in session:
-        return 'Logged in as %s' % escape(session['username'])
-    else:
-        return 'You are not logged in'
-
-@main.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
-    return '''
-        <form method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    '''
-
-@main.route('/logout')
-def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect(url_for('index'))
 
 @main.route('/about')
 def about():
